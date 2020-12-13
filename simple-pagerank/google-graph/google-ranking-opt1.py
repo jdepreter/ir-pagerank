4072e80551624b82fdf796b55767199d77966a81
jdepreter <jolan.depreter@gmail.com>
2020-12-13 14:56:09 +0100

    pyspark values at the top

--- simple-pagerank/google-graph/google-ranking-opt1.py
+++ simple-pagerank/google-graph/google-ranking-opt1.py
@@ -7,7 +7,10 @@ import pyspark
 from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.sql import SparkSession
 
-
+inputfile = "../../data/web-Google.txt"
+alpha = float(0.15)    # Teleportation probability
+epsilon = 0.000001      # 10^-6
+outputdir = "noout-alpha0.15.csv"
 
 config = pyspark.SparkConf().setAll([('spark.executor.memory', '8g'), ('spark.executor.cores', '4'), ('spark.cores.max', '4'), ('spark.driver.memory','8g')]) \
     .setAppName('appName').setMaster('local[*]')
@@ -25,12 +28,11 @@ spark = SparkSession(sc)
 
 
 # path = "/FileStore/shared_uploads/lander.deroeck@student.uantwerpen.be/web_Google.txt"
-path = "../../data/web-Google.txt"
 
 am_nodes = 875713
 am_edges = 5105039
 
-edges = sc.textFile(path)
+edges = sc.textFile(inputfile)
 edges = edges.filter(lambda edge: edge[0] != "#").map(lambda edge: tuple(edge.split("\t")))
 group_edges = edges.groupBy(lambda x: x[0])
 links = group_edges.map(lambda x: (x[0], [i[1] for i in x[1]]))
@@ -45,11 +47,6 @@ no_outgoing = outgoing.subtract(incoming)
 
 ranks_incoming = incoming.map(lambda x: (x, 1.0 / am_nodes))
 ranks_no_outgoing = no_outgoing.map(lambda x: (x, 1.0 / am_nodes))
-
-
-alpha = float(0.15)    # Teleportation probability
-
-epsilon = 0.000001 # 10^-6
 
 
 # base_ranks = nodes.map(lambda x: (x, alpha / am_nodes))
@@ -101,4 +98,4 @@ ranks = ranks_incoming.union(ranks_no_outgoing)
 ranks = ranks.sortBy(lambda node: -node[1])
 
 df = ranks.toDF()
-df.repartition(1).write.csv('noout-alpha0.15.csv')
+df.repartition(1).write.csv(outputdir)
--- simple-pagerank/google-graph/google-ranking.py
+++ simple-pagerank/google-graph/google-ranking.py
@@ -7,7 +7,10 @@ import pyspark
 from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.sql import SparkSession
 
-
+inputfile = "../../data/web-Google.txt"
+alpha = float(0.15)    # Teleportation probability
+epsilon = 0.000001      # 10^-6
+outputdir = "alpha0.15.csv"
 
 config = pyspark.SparkConf().setAll([('spark.executor.memory', '8g'), ('spark.executor.cores', '4'), ('spark.cores.max', '4'), ('spark.driver.memory','8g')]) \
     .setAppName('appName').setMaster('local[*]')
@@ -25,12 +28,10 @@ spark = SparkSession(sc)
 
 
 # path = "/FileStore/shared_uploads/lander.deroeck@student.uantwerpen.be/web_Google.txt"
-path = "../../data/web-Google.txt"
-
 am_nodes = 875713
 am_edges = 5105039
 
-edges = sc.textFile(path)
+edges = sc.textFile(inputfile)
 edges = edges.filter(lambda edge: edge[0] != "#").map(lambda edge: tuple(edge.split("\t")))
 group_edges = edges.groupBy(lambda x: x[0])
 links = group_edges.map(lambda x: (x[0], [i[1] for i in x[1]]))
@@ -42,12 +43,6 @@ outgoing = links.flatMap(lambda x: [i for i in x[1]])
 nodes = sc.union([incoming, outgoing]).distinct()
 
 ranks = nodes.map(lambda x: (x, 1.0 / am_nodes))
-
-
-alpha = float(0.15)    # Teleportation probability
-
-epsilon = 0.000001 # 10^-6
-
 
 base_ranks = nodes.map(lambda x: (x, alpha / am_nodes))
 print(base_ranks.filter(lambda x: x[0] == '876124').take(1) )
@@ -76,4 +71,4 @@ ranks = ranks.sortBy(lambda node: -node[1])
 print(ranks.take(10))
 
 df = ranks.toDF()
-df.repartition(1).write.csv('alpha0.15.csv')
+df.repartition(1).write.csv(outputdir)
